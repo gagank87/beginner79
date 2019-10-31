5d184b4aebcfc27fbda1ebfd3a5c5e34cc1935e9
Gagan Khanna <lakshaykapoor99@gmail.com>
2019-10-31 15:12:31 +0530

    First commit

---
+++ FIBONACC.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a=0,b=1,n,c,i;
+ clrscr();
+ printf("Enter the number of terms: ");
+ scanf("%d",&n);
+ printf("%d \n%d \n",a,b);
+ for(i=3;i<=n;i++)
+ {
+  c=a+b;
+  printf("%d \n",c);
+  a=b;
+  b=c;
+  }
+  getch();
+  }
